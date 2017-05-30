0b798b41d0367598ae582a1b610cc99b1f99a3fc
zhubojun <869502588@qq.com>
2017-05-30 09:44:29 +0800

    First commit

---
+++ lab3/serafini/Ngrams/src/ngrams.cpp
@@ -0,0 +1,14 @@
+// This is the CPP file you will edit and turn in.
+// Also remove these comments here and add your own.
+// TODO: remove this comment header
+
+
+using namespace std;
+
+int main() {
+    // TODO: Finish the program!
+
+
+    cout << "Exiting." << endl;
+    return 0;
+}
